547051e671c98883de050ac861719687841c9bf8
MKwapisz2000 <marcelinakwapisz920@gmail.com>
2025-06-03 09:49:43 +0200

    Dodano docstringi do utilis.py

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
+"""Moduł z prostymi operacjami matematycznymi."""  # <--- docstring modułu
+
 def add(a: int, b: int) -> int:
+    """Zwraca sumę a i b."""
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """Zwraca różnicę a i b."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Zwraca iloczyn a i b."""
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """Zwraca iloraz a i b."""
     return a / b
